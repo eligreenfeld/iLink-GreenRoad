39b9a511dedbed7b5557c551a8bf592bda665bfc
Eli Greenfeld <eli@greenco.tech>
2026-03-09 12:45:10 +0200

    Initialize Swift package structure for iLink

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "iLink",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "iLink",
+            targets: ["iLink"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "iLink",
+            path: "iLink",
+            resources: [
+                .process("iLink.bundle")
+            ],
+            publicHeadersPath: "."
+        )
+    ]
+)
